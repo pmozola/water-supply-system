09336e4355c693a8d86a4bb17a525ac1a04f0e90
adrianKas <adrian.kasperkiewicz@gmail.com>
2018-11-25 12:21:03 +0100

    Add new temperature controller

--- src/API/Wather.API/WaterSystemAPI/Controllers/TempController.cs
+++ src/API/Wather.API/WaterSystemAPI/Controllers/TempController.cs
@@ -3,43 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WaterSystemAPI.Models;
 
 namespace WaterSystemAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class TempController : ControllerBase
     {
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "trhftrjf", "value2" };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            Temperature temp = new Temperature();
+            
+            return Ok(temp);
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] Temperature value)
         {
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+      
     }
 }
---
+++ src/API/Wather.API/WaterSystemAPI/Models/Temperature.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WaterSystemAPI.Models
+{
+    public class Temperature
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double CelciusTemperature { get; set; }
+        public int ArduinoId { get; set; }
+    }
+}
