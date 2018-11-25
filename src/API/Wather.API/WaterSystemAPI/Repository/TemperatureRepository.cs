722b722f9ba34b4bc0cf2a6f6fb39e2d3ccd56c8
adrianKas <adrian.kasperkiewicz@gmail.com>
2018-11-25 12:35:02 +0100

    Add Temperature repository fake

---
+++ src/API/Wather.API/WaterSystemAPI/Repository/TemperatureRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WaterSystemAPI.Models;
+
+namespace WaterSystemAPI.Repository
+{
+    public class TemperatureRepository
+    {
+        private List<Temperature> TemperatureList;
+
+        void Add(Temperature temperature)
+
+        {
+            TemperatureList.Add(temperature);
+        }
+
+        Temperature Get (int id)
+
+        {
+         return   TemperatureList.FirstOrDefault(x => x.Id == id);
+        }
+
+
+    }
+}
