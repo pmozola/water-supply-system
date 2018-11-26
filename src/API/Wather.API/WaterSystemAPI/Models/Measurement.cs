16f3ef75ce3684d376a7cc4174c57fb942fa771e
mpawelm <mozola.pawel@gmail.com>
2018-11-26 11:34:05 +0100

    [Api] Common measurement model for arduino

---
+++ src/API/Wather.API/WaterSystemAPI/Models/Measurement.cs
@@ -0,0 +1,15 @@
+﻿namespace WaterSystemAPI.Controllers
+{
+    public class Measurement
+    {
+        public int ArduinoId  { get; set; }
+
+        public double Temperature { get; set; }
+
+        public double Humidity { get; set; }
+
+        public double SoilHumidity { get; set; }
+
+        public double LightIntensity { get; set; }
+    }
+}
